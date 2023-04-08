1df9de63f0655ac92bf36bf48cdaaf46982c60f1
Denis.hw.z3x <mail@z3x.ru>
2023-04-08 09:58:29 +0300

    Инпуты в отдельный компонент. Не доделал v2

---
+++ src/components/UI/myInput.vue
@@ -0,0 +1,25 @@
+<template>
+   <input type="text" :value="modelValue" @input="updateInput" >
+</template>
+
+<script>
+   export default {
+
+      props:{
+         mdoelValue:[String,Number]
+      },
+      methods: {
+         updateInput(event){
+            this.$emit('update:modelValue',event.target.value)
+         }
+      },
+   }
+</script>
+<style scoped>
+   input[type="text"]{
+      border:solid 1px rgb(67, 59, 140);
+      background-color: #eee;
+      padding:5px 10px;
+      border-radius: 5px;
+   }
+</style>
