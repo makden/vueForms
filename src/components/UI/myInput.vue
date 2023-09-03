282d6365247018ab383781f91019a6259f5539ed
Denis.hw.z3x <mail@z3x.ru>
2023-09-03 23:10:00 +0300

    Добавил стили, сделал скрытие. Надо сделать регисторо незаисис

--- src/components/UI/myInput.vue
+++ src/components/UI/myInput.vue
@@ -1,47 +1,70 @@
 <template>
-   <input type="text"  @input="updateInput" >
-   <ul v-for="arrVal in modelValue" >
-         <li> {{arrVal}}</li>
-         </ul>
-
-   <hr>
-   
-   <ul v-for="arrVal in arrVals" >
-         <li> {{arrVal}}</li>
-         </ul>
+   <div class="input_search">
+      <input type="text" :value="val" @input="updateInput">
+      <ul class="list_data" v-if="displayListData">
+         <li v-for="arrVal in arrVals" @click="selectItem"> {{ arrVal }}</li>
+      </ul>
+   </div>
 </template>
 
 <script>
-   export default {
-      name:'myInput',
-      props:{
-         modelValue:Array,
-         
-      },
-     data(){
+export default {
+   name: 'myInput',
+   props: {
+      modelValue: Array,
+
+   },
+   data() {
       return {
-         arrVals:Array
+         arrVals: Array,
+         val: "",
+         displayListData: false
       }
-     },
-      methods: {
-         updateInput(event){
-
-             this.arrVals = this.modelValue.filter(function (language) {
-              return language.startsWith(event.target.value)
-            })
-
-           
-            
-            //this.$emit('update:modelValue',event.target.value)
-         }
+   },
+   methods: {
+      updateInput(event) {
+         
+         this.arrVals = this.modelValue.filter(function (book) {
+            return book.indexOf(event.target.value) !== -1;
+         })
+         this.displayListData = this.arrVals.length <=3;
+         
+         this.val = event.target.value;
       },
-   }
+      selectItem(e) {
+         this.val = e.target.innerText
+         this.displayListData = false;
+      }
+   },
+}
 </script>
 <style scoped>
-   input[type="text"]{
-      border:solid 1px rgb(67, 59, 140);
-      background-color: #eee;
-      padding:5px 10px;
-      border-radius: 5px;
-   }
+.input_search {
+   border: solid 1px #def;
+   display: inline-block;
+}
+
+.input_search input[type="text"] {
+   border: solid 1px #aaa;
+   border-bottom: none;
+   background-color: #eee;
+   padding: 5px 10px;
+}
+
+ul.list_data {
+   margin: 0px;
+   padding: 0px;
+   list-style: none;
+   border: solid 1px #aaa;
+
+}
+
+ul.list_data li {
+   line-height: 22px;
+   padding: 5px;
+}
+
+ul.list_data li:hover {
+   background-color: #eee;
+}
 </style>
